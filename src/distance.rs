133b9c4e2bba62a48ea5a0f583861ed96b223a5a
Steve Yu <suguni@gmail.com>
2021-10-23 16:00:29 +0900

    print 함수 분리, Distance 에는 거리 정보만 들고 있게 변경, radix 36 rendering

--- src/distance.rs
+++ src/distance.rs
@@ -1,15 +1,13 @@
-use std::{collections::HashMap, fmt::Display};
+use std::collections::HashMap;
 
 use crate::grid::{Grid, Loc};
 
 pub struct Distance {
-    grid: Grid,
-    root: Loc,
     cells: HashMap<Loc, u32>,
 }
 
 impl Distance {
-    pub fn new(grid: Grid, root: Loc) -> Self {
+    pub fn from(grid: &Grid, root: Loc) -> Self {
         let mut cells = HashMap::new();
         cells.insert(root, 0);
 
@@ -33,69 +31,10 @@ impl Distance {
             frontier.append(&mut new_frontier);
         }
 
-        Self { grid, root, cells }
+        Self { cells }
     }
 
-    fn get(&self, loc: Loc) -> u32 {
+    pub fn get(&self, loc: Loc) -> u32 {
         *self.cells.get(&loc).unwrap()
-    }
-
-    // fn set(&mut self, loc: Loc, dist: u32) {
-    //     self.cells.insert(loc, dist);
-    // }
-
-    // fn cells(&self) -> std::collections::hash_map::Keys<Loc, u32> {
-    //     self.cells.keys()
-    // }
-}
-
-impl Display for Distance {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let mut output = String::new();
-
-        output.push('+');
-        for _ in 0..self.grid.width {
-            output.push_str("---+");
-        }
-        output.push('\n');
-
-        let mut top = String::new();
-        let mut bottom = String::new();
-
-        for row in 0..self.grid.height {
-            top.push('|');
-            bottom.push('+');
-
-            for col in 0..self.grid.width {
-                let cell = self.grid.cells[row * self.grid.width + col].borrow();
-
-                let dist = format!("{:^3}", self.get((row, col)));
-                top.push_str(dist.as_str());
-
-                if cell.east.is_some() && cell.linked(cell.east.as_ref().unwrap()) {
-                    top.push(' ');
-                } else {
-                    top.push('|');
-                }
-
-                if cell.south.is_some() && cell.linked(cell.south.as_ref().unwrap()) {
-                    bottom.push_str("   ");
-                } else {
-                    bottom.push_str("---");
-                }
-                bottom.push('+');
-            }
-
-            output.push_str(&top);
-            output.push('\n');
-
-            output.push_str(&bottom);
-            output.push('\n');
-
-            top.clear();
-            bottom.clear();
-        }
-
-        write!(f, "{}", output)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,62 @@ fn main() {
     // builder::binary_tree(&mut grid);
     builder::sidewinder(&mut grid);
 
-    let distance = distance::Distance::new(grid, (19, 0));
+    let distance = distance::Distance::from(&grid, (19, 0));
 
-    println!("{}", distance);
+    render(&grid, &distance);
 
     // let doc = export_svg::to_svg(grid, 10);
 
     // svg::save("image.svg", &doc).unwrap();
+}
+
+fn render(grid: &grid::Grid, distance: &distance::Distance) {
+    use radix_fmt::radix;
+
+    let mut output = String::new();
+
+    output.push('+');
+    for _ in 0..grid.width {
+        output.push_str("---+");
+    }
+    output.push('\n');
+
+    let mut top = String::new();
+    let mut bottom = String::new();
+
+    for row in 0..grid.height {
+        top.push('|');
+        bottom.push('+');
+
+        for col in 0..grid.width {
+            let cell = grid.cells[row * grid.width + col].borrow();
+
+            let dist = format!("{:^3}", radix(distance.get((row, col)), 36).to_string());
+            top.push_str(dist.as_str());
+
+            if cell.east.is_some() && cell.linked(cell.east.as_ref().unwrap()) {
+                top.push(' ');
+            } else {
+                top.push('|');
+            }
+
+            if cell.south.is_some() && cell.linked(cell.south.as_ref().unwrap()) {
+                bottom.push_str("   ");
+            } else {
+                bottom.push_str("---");
+            }
+            bottom.push('+');
+        }
+
+        output.push_str(&top);
+        output.push('\n');
+
+        output.push_str(&bottom);
+        output.push('\n');
+
+        top.clear();
+        bottom.clear();
+    }
+
+    println!("{}", output)
 }
